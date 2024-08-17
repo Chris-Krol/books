4ab31813cb6b592e5091609dc78d5f0041a08a5f
Chris <chriskrol97@gmail.com>
2024-08-17 15:16:02 -0700

    Finished all of the routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 import axios from "axios";
@@ -13,6 +13,10 @@ function App() {
     setBooks(response.data);
   };
 
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
   const createBook = async (title) => {
     
     const response = await axios.post('http://localhost:3001/books', {
@@ -23,17 +27,24 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete('http://localhost:3001/books/' + id);
+
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
     setBooks(updatedBooks);
   };
 
-  const onEdit = (id, newTitle) => {
+  const onEdit = async (id, newTitle) => {
+
+    const response = await axios.put('http://localhost:3001/books/' + id, {
+      title: newTitle
+    });
+
     const updatedBooks = books.map((book) => {
       if(book.id === id){
-        return {...book, title: newTitle};
+        return {...book, ...response.data};
       }
       return book;
     });
