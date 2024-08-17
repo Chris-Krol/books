2755210487f0c0e5630f326a1780c92d3a1aa6c9
Chris <chriskrol97@gmail.com>
2024-08-17 14:38:05 -0700

    Adding a db

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import axios from "axios";
 
 
 function App() {
   
   const [books, setBooks] = useState([]);
+  
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books');
+    setBooks(response.data);
+  };
 
-  const createBook = (title) => {
-    const updatedBooks= [...books, {id: Math.round(Math.random() * 99999), title}];
+  const createBook = async (title) => {
+    
+    const response = await axios.post('http://localhost:3001/books', {
+      title
+    });
+    
+    const updatedBooks= [...books, response.data];
     setBooks(updatedBooks);
   };
 
