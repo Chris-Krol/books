9c5434585bb4f6b06338cdf80d9e59cc9a6aa977
Chris Krol <ckrol@simplic-it.com>
2024-08-16 21:40:53 -0700

    initial commit and setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
 
 function App() {
+  
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log('need to add book with title', title);
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BookCreate onCreate={createBook} />
     </div>
   );
 }
