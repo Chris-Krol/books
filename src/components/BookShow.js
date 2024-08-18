8d58829f8a6ec929c3562cc9e05b5b18b5676e81
Chris <chriskrol97@gmail.com>
2024-08-17 23:09:35 -0700

    Added custom hook to simplify imports

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,10 +1,10 @@
-import { useState, useContext } from "react";
+import { useState } from "react";
 import BookEdit from "./BookEdit";
-import BooksContext from "../context/books";
+import useBooksContext from "../hooks/use-books-context";
 
 function BookShow({ book }) {
 
-  const { deleteBookById } = useContext(BooksContext);
+  const { deleteBookById } = useBooksContext();
 
   const [showEdit, setShowEdit] = useState(false);
 
