e6941600840ea441e21febaaa36353235e7188f6
Chris <chriskrol97@gmail.com>
2024-08-17 22:59:01 -0700

    Added context to the app

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,19 @@
+import { useContext } from "react";
+import BooksContext from "../context/books";
 import BookShow from "./BookShow";
 
-function BookList({ books, onDelete, onEdit}) {
+function BookList() {
+
+  const { books } = useContext(BooksContext);
+
   const renderedBooks = books.map((book) => {
-    return <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />;
+    return <BookShow key={book.id} book={book}/>;
   });  
   
   return (
-      <div className='book-list'>{renderedBooks}</div>
+      <div className='book-list'>
+        {renderedBooks}
+      </div>
     );
   }
   
--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,21 +1,23 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import BookEdit from "./BookEdit";
+import BooksContext from "../context/books";
 
-function BookShow({ book, onDelete, onEdit}) {
+function BookShow({ book }) {
+
+  const { deleteBookById } = useContext(BooksContext);
 
   const [showEdit, setShowEdit] = useState(false);
 
   const handleDeleteClick = () => {
-    onDelete(book.id);
+    deleteBookById(book.id);
   };
 
   const handleEditClick = () => {
     setShowEdit(!showEdit);
   };
 
-  const handleSubmit = (id, newTitle) => {
+  const handleSubmit = () => {
     setShowEdit(false);
-    onEdit(id, newTitle);
   };
 
   let content = <h3>{book.title}</h3>
